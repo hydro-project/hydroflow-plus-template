eb4e98b564519aa0c50ddd03540940147d01aa81
Mingwei Samuel <mingwei.samuel@gmail.com>
2024-06-18 19:42:33 -0700

    fixup! fixup! fix: for upstream changes

--- flow/src/bin/first_ten_distributed.rs
+++ flow/src/bin/first_ten_distributed.rs
@@ -1,6 +1,6 @@
 #[tokio::main]
 async fn main() {
-    hydroflow_plus::util::cli::launch(|ports| {
+    hydroflow_plus::util::cli::launch!(|ports| {
         flow::first_ten_distributed::first_ten_distributed_runtime!(&ports)
     })
     .await;
